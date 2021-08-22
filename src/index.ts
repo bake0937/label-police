34d63f5360ddec89311e071b96454c01a18cdc53
bake0937 <sadayasu.info@gmail.com>
2021-08-23 03:20:59 +0900

    コードレビュー時にラベルを貼る作業を機能化

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,153 @@
 import { Probot } from "probot";
 
 export = (app: Probot) => {
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    await context.octokit.issues.createComment(issueComment);
-  });
-  // For more information on building apps:
-  // https://probot.github.io/docs/
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+  /**
+   * レビューイが pull request を open すると「WIP」ラベルを貼る．
+   */
+  app.on("pull_request.opened", async (context) => {
+    const labelParams = context.issue({
+      labels: ['WIP'],
+    });
+    await context.octokit.issues.addLabels(labelParams);
+  });
+
+  /**
+   * レビューイが Conversation で「レビューお願いします」とコメントすると「Needs review」ラベルを貼る．
+   */
+  app.on("issue_comment.created", async (context) => {
+    if (context.payload.issue.html_url.includes("pull") && context.payload.comment.body.includes('レビューお願いします') && context.payload.issue.user.login === context.payload.comment.user.login) {
+
+      if (context.payload.issue.labels.some(label => label.name.includes('WIP'))) {
+        const removeLabelParams = context.issue({
+          name: 'WIP',
+        });
+        await context.octokit.issues.removeLabel(removeLabelParams);
+      };
+
+      const labelParams = context.issue({
+        labels: ['Needs review'],
+      });
+      await context.octokit.issues.addLabels(labelParams);
+    }
+  });
+
+  /**
+   * レビューイが discussion で「レビューお願いします」とコメントすると「Needs review」ラベルを貼る．
+   */
+  app.on("pull_request_review_comment.created", async (context) => {
+    if (context.payload.comment.html_url.includes("pull") && context.payload.comment.body.includes('レビューお願いします') && context.payload.comment.user.login === context.payload.comment.user.login) {
+
+      if (context.payload.pull_request.labels.some(label => label.name.includes('WIP'))) {
+        const removeLabelParams = context.issue({
+          name: 'WIP',
+        });
+        await context.octokit.issues.removeLabel(removeLabelParams);
+      };
+
+      const labelParams = context.issue({
+        labels: ['Needs review'],
+      });
+      await context.octokit.issues.addLabels(labelParams);
+    }
+  });
+
+  /**
+   * レビューイが Review で「レビューお願いします」とコメントすると「Needs review」ラベルを貼る．
+   */
+  app.on("pull_request_review.submitted", async (context) => {
+    if (context.payload.review.html_url.includes("pull") && context.payload.review.state === 'commented' && context.payload.review.user.login === context.payload.pull_request.user.login) {
+
+      if (context.payload.pull_request.labels.some(label => label.name.includes('WIP'))) {
+        const removeLabelParams = context.issue({
+          name: 'WIP',
+        });
+        await context.octokit.issues.removeLabel(removeLabelParams);
+      };
+
+      const labelParams = context.issue({
+        labels: ['Needs review'],
+      });
+      await context.octokit.issues.addLabels(labelParams);
+    };
+  });
+
+  /**
+   * レビュアーが Request changes を実行した場合は「WIP」ラベルを貼る．
+   */
+  app.on("pull_request_review.submitted", async (context) => {
+    if (context.payload.review.state === 'changes_requested') {
+      if (context.payload.pull_request.labels.some(label => label.name.includes('Needs review'))) {
+        const removeLabelParams = context.issue({
+          name: 'Needs review',
+        });
+        await context.octokit.issues.removeLabel(removeLabelParams);
+      };
+
+      const labelParams = context.issue({
+        labels: ['WIP'],
+      });
+      await context.octokit.issues.addLabels(labelParams);
+    };
+  });
+
+  /**
+   * レビューイが pull request を reopen すると「WIP」ラベルを貼る．
+   */
+  app.on("pull_request.reopened", async (context) => {
+    if (context.payload.pull_request.labels.some(label => label.name.includes('Close pull request'))) {
+      const removeLabelParams = context.issue({
+        name: 'Close pull request',
+      });
+      await context.octokit.issues.removeLabel(removeLabelParams);
+    };
+    const labelParams = context.issue({
+      labels: ['WIP'],
+    });
+    await context.octokit.issues.addLabels(labelParams);
+  });
+
+  /**
+   * レビューイが pull request をマージすると全てのラベルを剥がし，「Reviewed」ラベルを貼る，
+   * レビューイが pull request を closed すると「Close pull request」ラベルを貼る．
+   */
+  app.on("pull_request.closed", async (context) => {
+
+    let setLabelParams : {issue_number: any;} & {owner: string; repo: string;} & {name: string;}
+    let closeLabelParams : {issue_number: any;} & {owner: string; repo: string;} & {labels: string[];}
+
+    switch (context.payload.pull_request.merged){
+      case true:
+        setLabelParams = context.issue({
+          name: 'Reviewed',
+        });
+        await context.octokit.issues.setLabels(setLabelParams);
+        break;
+      default:
+        if (context.payload.pull_request.labels.some(label => label.name.includes('Needs review'))) {
+          const removeLabelParams = context.issue({
+            name: 'Needs review',
+          });
+          await context.octokit.issues.removeLabel(removeLabelParams);
+        };
+
+        if (context.payload.pull_request.labels.some(label => label.name.includes('WIP'))) {
+          const removeLabelParams = context.issue({
+            name: 'WIP',
+          });
+          await context.octokit.issues.removeLabel(removeLabelParams);
+        };
+
+        closeLabelParams = context.issue({
+          labels: ['Close pull request'],
+        });
+        await context.octokit.issues.addLabels(closeLabelParams);
+        break;
+    }
+  });
+
+  // for Debug
+  // app.onAny(async (context) => {
+  //   app.log.info({ event: context.name, action: context.payload.action, context });
+  // });
 };
